f37756bfd08cfebb63eaf6613325215237db3235
btran369 <VuAnhQuan.Tran@georgebrown.ca>
2025-01-12 21:55:33 -0500

    Lab 1 Python Codes

---
+++ Labs/Lab1/lab1.py
@@ -0,0 +1,17 @@
+# Sample Coding Questions 01 Week 01
+# Vu Anh Quan Tran
+# Defining array var
+random_nums = [1,4,7,9]
+# Order of Ops
+a = 1
+b = 2
+c = 3
+d = 4
+e = a - ((b ** c) // d) + (a % c)
+# Formatting
+temperature = 32.6
+to_print = f"The temperature today is: {temperature:.3f} degrees Celsius"
+print(to_print)
+# I/O Pracs
+user_age = input("Enter your age: ")
+print(f"Now showing items fitlered by age: {user_age}")
